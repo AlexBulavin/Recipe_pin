9413433c378ade1d53e0ea72cd93f0565f4d4e22
AlexBulavin <45867076+AlexBulavin@users.noreply.github.com>
2020-03-19 08:43:10 +0300

    Приложение к уроку 9 SkyUniversity Swift

---
+++ FoodPin/RecipesTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  RecipesTableViewController.swift
+//  FoodPin
+//
+//  Created by Alex on 19.03.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class RecipesTableViewController: UITableViewController {
+
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery" , "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Ko ng", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", " British", "Thai"]
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellIdentifier = "datacell"
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RecipeTableViewCell
+    // Configure the cell...
+    cell.nameLabel?.text = restaurantNames[indexPath.row]
+    cell.thumbnailImageView?.image = UIImage(named: restaurantImages[indexPath.row])
+    cell.locationLabel?.text = restaurantLocations[indexPath.row]
+    cell.typeLabel?.text = restaurantTypes[indexPath.row]
+        
+    return cell }
+
+    override func numberOfSections(in tableView: UITableView) -> Int { return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return restaurantNames.count }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
